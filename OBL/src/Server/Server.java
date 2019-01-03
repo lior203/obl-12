0ec664ef191b74ec44dd082cd05dea28c6ff1a68
idan9532 <46168838+idan9532@users.noreply.github.com>
2019-01-03 20:07:25 +0200

    server change

--- OBL/src/Server/Server.java
+++ OBL/src/Server/Server.java
@@ -9,126 +9,128 @@ import java.sql.*;
 import ocsf.server.*;
 
 /**
-* This class overrides some of the methods in the abstract 
-* superclass in order to give more functionality to the server.
-*
-* @author Dr Timothy C. Lethbridge
-* @author Dr Robert Lagani&egrave;re
-* @author Fran&ccedil;ois B&eacute;langer
-* @author Paul Holden
-* @version July 2000
-*/
+ * This class overrides some of the methods in the abstract 
+ * superclass in order to give more functionality to the server.
+ *
+ * @author Dr Timothy C. Lethbridge
+ * @author Dr Robert Lagani&egrave;re
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Paul Holden
+ * @version July 2000
+ */
 public class Server extends AbstractServer 
 {
-//Class variables *************************************************
+	//Class variables *************************************************
 
-/**
-* The default port to listen on.
-*/
-final public static int DEFAULT_PORT = 5555;
-public static Connection conn;
-//Constructors ****************************************************
+	/**
+	 * The default port to listen on.
+	 */
+	final public static int DEFAULT_PORT = 5555;
+	public static Connection conn;
+	//Constructors ****************************************************
 
-/**
-* Constructs an instance of the echo server.
-*
-* @param port The port number to connect on.
-*/
-public Server(int port) 
-{
- super(port);
-}
+	/**
+	 * Constructs an instance of the echo server.
+	 *
+	 * @param port The port number to connect on.
+	 */
+	public Server(int port) 
+	{
+		super(port);
+	}
 
-//Instance methods ************************************************
+	//Instance methods ************************************************
 
-/**
-* This method handles any messages received from the client.
-*
-* @param msg The message received from the client.
-* @param client The connection from which the message originated.
-*/
-public void handleMessageFromClient (Object msg, ConnectionToClient client) {
-	    System.out.println("Message received: " + msg + " from " + client);
-	    
-	    
+	/**
+	 * This method handles any messages received from the client.
+	 *
+	 * @param msg The message received from the client.
+	 * @param client The connection from which the message originated.
+	 */
+	public void handleMessageFromClient (Object msg, ConnectionToClient client) {
+		System.out.println("Message received: " + msg + " from " + client);
+
+
 		this.sendToAllClients(msg);
-}
+	}
 
- 
-/**
-* This method overrides the one in the superclass.  Called
-* when the server starts listening for connections.
-*/
-protected void serverStarted()
-{
- System.out.println
-   ("Server listening for connections on port " + getPort());
-}
 
-/**
-* This method overrides the one in the superclass.  Called
-* when the server stops listening for connections.
-*/
-protected void serverStopped()
-{
- System.out.println
-   ("Server has stopped listening for connections.");
-}
+	/**
+	 * This method overrides the one in the superclass.  Called
+	 * when the server starts listening for connections.
+	 */
+	protected void serverStarted()
+	{
+		System.out.println
+		("Server listening for connections on port " + getPort());
+	}
 
-//Class methods ***************************************************
+	/**
+	 * This method overrides the one in the superclass.  Called
+	 * when the server stops listening for connections.
+	 */
+	protected void serverStopped()
+	{
+		System.out.println
+		("Server has stopped listening for connections.");
+	}
 
-/**
-* This method is responsible for the creation of 
-* the server instance (there is no UI in this phase).
-*
-* @param args[0] The port number to listen on.  Defaults to 5555 
-*          if no argument is entered.
-*/
-public static void main(String[] args) 
-{
- int port = 0; //Port to listen on
+	//Class methods ***************************************************
 
- try
- {
-   port = Integer.parseInt(args[0]); //Get port from command line
- }
- catch(Throwable t)
- {
-   port = DEFAULT_PORT; //Set port to 5555
- }
-	
- Server sv = new Server(port);
- 
- try 
- {
-   sv.listen(); //Start listening for connections
- } 
- catch (Exception ex) 
- {
-   System.out.println("ERROR - Could not listen for clients!");
- }
- conn = sv.connectToDatabase();
+	/**
+	 * This method is responsible for the creation of 
+	 * the server instance (there is no UI in this phase).
+	 *
+	 * @param args[0] The port number to listen on.  Defaults to 5555 
+	 *          if no argument is entered.
+	 */
+
+	private Connection connectToDatabase() {
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (Exception ex) {/* handle the error*/}
+
+		try 
+		{
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/obl","root","Aa123456");
+			System.out.println("SQL connection succeed");
+			return conn;
+		} catch (SQLException ex) 
+		{	/* handle any errors*/
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		return null;
+	}
+
+	public static void main(String[] args) 
+	{
+		int port = 0; //Port to listen on
+
+		try
+		{
+			port = Integer.parseInt(args[0]); //Get port from command line
+		}
+		catch(Throwable t)
+		{
+			port = DEFAULT_PORT; //Set port to 5555
+		}	
+		Server sv = new Server(port);
+		conn = sv.connectToDatabase();
+		try 
+		{
+			sv.listen(); //Start listening for connections
+		} 
+		catch (Exception ex) 
+		{
+			System.out.println("ERROR - Could not listen for clients!");
+		}
+
+		
+	}
 }
 
 //My Methods
-private Connection connectToDatabase() {
-	  try 
-		{
-       Class.forName("com.mysql.jdbc.Driver").newInstance();
-   } catch (Exception ex) {/* handle the error*/}
-   
-   try 
-   {
-       Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/obl","root","Aa123456");
-       System.out.println("SQL connection succeed");
-       return conn;
-	} catch (SQLException ex) 
-	    {	/* handle any errors*/
-       System.out.println("SQLException: " + ex.getMessage());
-       System.out.println("SQLState: " + ex.getSQLState());
-       System.out.println("VendorError: " + ex.getErrorCode());
-       }
-	return null;
-	}
-}
-//End of Server class
+
