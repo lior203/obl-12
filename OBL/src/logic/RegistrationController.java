18b9ff59fe9be43e5775409b48cc57a24f23863a
idan9532 <46168838+idan9532@users.noreply.github.com>
2019-01-04 10:47:50 +0200

    commit

--- OBL/src/logic/RegistrationController.java
+++ OBL/src/logic/RegistrationController.java
@@ -1,11 +1,24 @@
 package logic;
 
+import java.util.ArrayList;
+
+import Client.Client;
+
 public class RegistrationController {
 
-
+	Client client;
 
 	public  static void registration(String phoneNumber,String id,String lastName,String firstName,String Email, String password)
 	{
+		System.out.println(phoneNumber);
+		ArrayList<String> registrationData = new ArrayList<>();
+		registrationData.add("Registration");
+		registrationData.add(phoneNumber);
+		registrationData.add(id);
+		registrationData.add(lastName);
+		registrationData.add(firstName);
+		registrationData.add(Email);
+		registrationData.add(password);
 
 	}
 
