f63e8fe7d351047f060c680234792ba9c2e90dd5
lior203 <46168715+lior203@users.noreply.github.com>
2019-01-04 10:42:06 +0200

    Main

--- OBL/src/logic/Main.java
+++ OBL/src/logic/Main.java
@@ -1,5 +1,6 @@
-package GUI;
-	
+package logic;
+
+import GUI.OBLcontroller;
 import GUI.RegistrationController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,12 +11,22 @@ import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+
+	final public static int DEFAULT_PORT = 5555;
+	public static String host;
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		try {
+			this.host = getParameters().getRaw().get(0);
+		}
+		catch(Exception e) {
+			this.host = "localhost";
+		}
 		OBLcontroller obl=new OBLcontroller();
 		obl.start(primaryStage);
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
