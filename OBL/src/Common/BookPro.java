8fcda9aaee504f7c3d5f35ee1c014eb58f6db275
lior203 <46168715+lior203@users.noreply.github.com>
2019-01-28 16:25:40 +0200

    Revert "lior"
    This reverts commit 3b498f339ab0063e786f74c0be02dfa4e4f3f0b1.

--- OBL/src/Common/BookPro.java
+++ OBL/src/Common/BookPro.java
@@ -1,5 +1,7 @@
 package Common;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
